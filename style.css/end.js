f520091aef02ae5ea2de9a4eccca564eb35c61b7
Patricia Hinkson <wassyman@hotmail.com>
2021-08-29 23:03:20 -0400

    ready

--- style.css/end.js
+++ style.css/end.js
@@ -1,1 +1,30 @@
 end.js
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = localStorage.getItem('#mostRecentScore')
+
+const highScore = JSON.parse(localStorage.getItem('highScore')) || []
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+    highScore.push(score)
+
+    highscore.sort((a, b) => {
+        return b.score - a.score
+    })
+    highScore.splice(5)
+
+    localStorage.setItem('highscores', JSON.stringify(highscore))
+    Window.location.assign('/')
+}
